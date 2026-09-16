416a95ab751ef0c4e2ff0d30a5fec6393152c242
kikai0724 <kikai.tennsaidayo@gmail.com>
2026-09-16 11:42:29 +0000

    hotfix: prevent null pickup from missing item definitions

--- 17.30/framework.h
+++ 17.30/framework.h
@@ -280,6 +280,12 @@ T* Actors(UClass* Class = T::StaticClass(), FVector Loc = {}, FRotator Rot = {},
 
 AFortPickup* SpawnPickup(UFortItemDefinition* ItemDef, int OverrideCount, int LoadedAmmo, FVector Loc, EFortPickupSourceTypeFlag SourceType, EFortPickupSpawnSource Source, bool bShouldCombine = false, AFortPawn* Pawn = nullptr)
 {
+	if (!ItemDef) {
+		// Missing item definition (hotfix/data mismatch). Avoid spawning null pickup to prevent crashes.
+		Log("SpawnPickup: ItemDef is null, skipping spawn.");
+		return nullptr;
+	}
+
 	auto SpawnedPickup = Actors<AFortPickup>(AFortPickup::StaticClass(), Loc);
 	SpawnedPickup->bRandomRotation = true;
 
